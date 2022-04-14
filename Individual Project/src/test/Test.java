18699992322b25b1a696f803554448442024df62
A klein <akendo21@gmail.com>
2022-04-13 22:31:10 -0500

    Initial commits to set up structure

---
+++ Individual Project/src/test/Test.java
@@ -0,0 +1,15 @@
+/*
+    File: Test.java
+    Author: Alex Klein
+    Date: 04/13/2022
+    Description: This will be a test for the program
+
+ */
+
+package test.java;
+
+public class Test {
+
+
+
+}
