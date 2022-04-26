a752f38d8e4923c65854a967c5fa89d06abc696b
A klein <akendo21@gmail.com>
2022-04-26 01:27:54 -0500

    Rearraging main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,18 +13,20 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
 
     public static void main(String[] args) {
-
+        //Create hash maps for storage
         HashMap<Integer, Student> studentHashMap = new HashMap<Integer, Student>();
         HashMap<Integer, Staff> staffHashMap = new HashMap<Integer,Staff>();
         HashMap<Integer, String> fNameReference = new HashMap<Integer, String>();
         HashMap<Integer, String> lNameReference = new HashMap<Integer, String>();
-
+        int student_cap;
+        int staff_cap;
+        //Generate a new standard school with rooms
         System.out.println("Starting by generating the school");
         StandardSchool standardSchool = new StandardSchool();
         Director director = new Director(standardSchool);
-
-        int student_cap = standardSchool.getTotalStudentCapacity();
-        int staff_cap = standardSchool.getMinimumStaffRequirements();
+        //Pull capacities
+        student_cap = standardSchool.getTotalStudentCapacity();
+        staff_cap = standardSchool.getMinimumStaffRequirements();
 
         System.out.println("Student capacity is " + student_cap);
         System.out.println("Staff capacity is " + staff_cap);
